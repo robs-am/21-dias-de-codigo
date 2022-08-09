13a5cc416ee2c088a87a63e909bc3934b2c96c6c
robs-am <roberta.amaro89@gmail.com>
2022-08-09 19:41:30 -0300

    add const slide e slideIcon

--- portinary gallery 2/js/main.js
+++ portinary gallery 2/js/main.js
@@ -21,3 +21,5 @@ tema.addEventListener('click', () => {
 
 const nextBtn = document.querySelector(".next-btn");
 const prevBtn = document.querySelector(".prev-btn");
+const slides = document.querySelectorAll(".slide");
+const slideIcons = document.querySelectorAll(".slide-icon");
