839deede8fcca9e03130adc2a80a778d739ed338
robs-am <roberta.amaro89@gmail.com>
2022-08-11 20:59:47 -0300

    reorganizing files

---
+++ Pilotando a DOM/js/elementos.js
@@ -0,0 +1,37 @@
+//getElementById() pega todo o conteúdo: h1, tittle, etc //
+
+/* const element = document.getElementById('blog-title')
+console.log(element) */
+
+
+
+//getElementByClassName() - seleciona diversos elementos 
+//que possuem a mesma classe//
+
+/* const element = document.getElementsByClassName();
+console.log(element) */
+
+
+
+//getElementByTagName() - h1, body, meta, head...
+
+/* const element = document.getElementsByTagName('body')
+console.log(element) */
+
+
+//querySelector - faz uma pesquisa pelo selector css, encontra o primeiro que acha
+
+/* const element = document.querySelector('.one')
+console.log(element) */
+
+//querySelectorAll - faz uma pesquisa por todos os seletores css no HTML (colecao de nós)
+
+//const element = document.querySelectorAll('#one')
+
+ //Qual usar?
+ //getElementById (element)
+ //getElementsByClassName (HTMLCollection)
+ //getElement s ByTagName (HTMLCollection)
+ //querySelector (element) //prefrencial
+ //querySelectorAll (Nodelist) //prefrencial
+
