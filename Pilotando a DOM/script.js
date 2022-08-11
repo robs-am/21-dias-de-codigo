d6225e5177b74c18b570720a9387991b7109cea4
robs-am <roberta.amaro89@gmail.com>
2022-08-11 19:56:08 -0300

    add getElementsByClassName

--- Pilotando a DOM/script.js
+++ Pilotando a DOM/script.js
@@ -1,3 +1,10 @@
-//getElementById() pega todo o conteúdo //
+//getElementById() pega todo o conteúdo: h1, tittle, etc //
 
-const element = document.getElementById('blog-title')
+/* const element = document.getElementById('blog-title')
+console.log(element) */
+
+//getElementByClassName() - seleciona diversos elementos 
+//que possuem a mesma classe//
+
+const element = document.getElementsByClassName();
+console.log(element)
