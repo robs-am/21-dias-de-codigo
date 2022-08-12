3ce414bdffeb40fe3df0440f71b8b05d886deae6
robs-am <roberta.amaro89@gmail.com>
2022-08-11 21:04:54 -0300

    add innertext

---
+++ Pilotando a DOM/js/mConteudo.js
@@ -0,0 +1,10 @@
+//Manipulando conteúdo
+
+
+//textContent - altera ou adiciona elementos
+//const element = document. querySelector('h1') - captura o elemento
+//element.textContent += "Alooow!" - faz a alteração, nesse caso adicionará ao h1
+
+
+
+//innertext
