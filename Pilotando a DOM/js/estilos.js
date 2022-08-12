0d1cb7fc2ff2c6fa70253f6eda295b0d9f4086cd
robs-am <roberta.amaro89@gmail.com>
2022-08-12 13:41:52 -0300

    add estilos.html

---
+++ Pilotando a DOM/js/estilos.js
@@ -0,0 +1,15 @@
+//Alterando estilos
+// Atribui CSS ao Javascript
+
+//const element = document.querySelector('body')
+
+//element.style.backgroundColor = "#F9F3D2"
+//console.log(element.backgroundColor)
+
+
+
+//classList
+
+const element = document.querySelector('body')
+element.classList.add('active', 'green')
+console.log(element.classList)
