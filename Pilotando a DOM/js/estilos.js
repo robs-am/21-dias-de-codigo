2f02321eee80e71c39f935739384c2504cdb8c55
robs-am <roberta.amaro89@gmail.com>
2022-08-12 17:07:46 -0300

    add classList

--- Pilotando a DOM/js/estilos.js
+++ Pilotando a DOM/js/estilos.js
@@ -11,5 +11,10 @@
 //classList
 
 const element = document.querySelector('body')
-element.classList.add('active', 'green')
+element.classList.add('active', 'green') //adiciona classes
+
 console.log(element.classList)
+
+element.classList.remove('active') //remove a classe selecionada
+
+element.classList.toggle('active') //ele pesquisa e se a classe existir ele remove, se não existir, ele adiciona(funciona como um interruptor on/off)
