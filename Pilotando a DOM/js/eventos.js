05086ea20ae2b47b9368495f6638626921d31746
robs-am <roberta.amaro89@gmail.com>
2022-08-22 16:56:38 -0300

    add onkey events

--- Pilotando a DOM/js/eventos.js
+++ Pilotando a DOM/js/eventos.js
@@ -1,5 +1,12 @@
 //Eventos
-function print() {
-    console.log('print')
-}
+//function print() {
+    //console.log('print')
+//}
 //toda vez que clicar aparecerá um print no console
+
+//onkeydown , onkeyup, onekeypress 
+//const input = document.querySelector('input');
+
+//input.onkeypress = function() {
+    //console.log('rodei')
+//}
