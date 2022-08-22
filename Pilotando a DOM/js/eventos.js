852efa53d3d8709e0bcb104ac7c9af1619511481
robs-am <roberta.amaro89@gmail.com>
2022-08-22 16:59:15 -0300

    add EventListener

--- Pilotando a DOM/js/eventos.js
+++ Pilotando a DOM/js/eventos.js
@@ -10,3 +10,14 @@
 //input.onkeypress = function() {
     //console.log('rodei')
 //}
+
+
+//Adicionando Eventos
+
+const h1 = document.querySelector('h1');
+
+h1.addEventListener('click', print)
+
+function print() {
+    console.log('print')
+}
