84c5ddb7696a2909c4e8ea7926b937891f4874b7
robs-am <roberta.amaro89@gmail.com>
2022-08-22 17:13:42 -0300

    add funcionalidade event

--- Pilotando a DOM/js/eventos.js
+++ Pilotando a DOM/js/eventos.js
@@ -1,8 +1,13 @@
 //Eventos
+
+
 //function print() {
     //console.log('print')
 //}
 //toda vez que clicar aparecerá um print no console
+
+
+
 
 //onkeydown , onkeyup, onekeypress 
 //const input = document.querySelector('input');
@@ -12,12 +17,40 @@
 //}
 
 
+
+
+
 //Adicionando Eventos
 
-const h1 = document.querySelector('h1');
+//const h1 = document.querySelector('h1');
 
+//h1.addEventListener('click', print)
+
+//function print() {
+    //console.log('print')
+//}
+
+
+
+//Outro método adicionando mais de um evento
+
+/* const h1 = document.querySelector('h1');
 h1.addEventListener('click', print)
 
 function print() {
     console.log('print')
+}
+
+h1.addEventListener('click', function(){
+    console.log('outro momento')
+})  */
+
+//Nesse exemplo em um clique ele executará as  funcoes 
+
+
+//argumento Event
+
+const input = document.querySelector('input')
+input.onkeypress = function(event){
+    console.log(event)
 }
