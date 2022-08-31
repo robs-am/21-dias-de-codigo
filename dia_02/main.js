c4271d36f06f9621966e7f9d44ef6d75bcb7f85f
robs-am <roberta.amaro89@gmail.com>
2022-08-31 20:39:53 -0300

    add background

--- dia_02/main.js
+++ dia_02/main.js
@@ -1,1 +1,10 @@
+var i = 0;
+var txt = " Hello, World!";
+var speed = 150;
 
+function type() {
+    document.getElementById("type").innerHTML += txt.charAt(i);
+    i++
+    setTimeout(type, speed);
+}
+type();
