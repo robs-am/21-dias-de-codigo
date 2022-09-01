b632cd0f327c67ed2937167447608cc1a04853d1
robs-am <roberta.amaro89@gmail.com>
2022-08-31 22:22:59 -0300

    add loop for each

--- dia_02/main.js
+++ dia_02/main.js
@@ -1,10 +1,17 @@
 var i = 0;
-var txt = " Hello, World!";
+var txt = "Hello, World!";
 var speed = 150;
 
-function type() {
-    document.getElementById("type").innerHTML += txt.charAt(i);
-    i++
-    setTimeout(type, speed);
+
+for (let index = 0; index < txt.length; index++) {
+	const element = txt[index];
+	function type() {
+	    document.getElementById("type").innerHTML+=txt.charAt(i);
+	    i++;
+	    setTimeout(type,speed);
+	}
 }
+
 type();
+
+
