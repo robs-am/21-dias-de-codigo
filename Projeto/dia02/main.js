3ed1d16ac6000edfa18c67520e299d01974ac563
robs-am <roberta.amaro89@gmail.com>
2022-09-06 00:14:00 -0300

    add new folder projeto

---
+++ Projeto/dia02/main.js
@@ -0,0 +1,21 @@
+var i = 0;
+var txt = "Hello, World!";
+var speed = 80;
+var delayInMilliseconds = 1000; 
+// segundos delay
+
+
+setTimeout( function() {
+
+	for (let index = 0; index < txt.length; index++) {
+		
+		function type() {
+			document.getElementById("type").innerHTML+=txt.charAt(i);
+			i++;
+			setTimeout(type,speed);
+		}
+	}
+
+	type();
+
+} , delayInMilliseconds);
