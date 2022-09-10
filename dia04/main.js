a2ef3513980cd1e47cf2c9c767eb9ac96f97a164
robs-am <roberta.amaro89@gmail.com>
2022-09-10 12:21:10 -0300

    add dia04 class calculator && clear

--- dia04/main.js
+++ dia04/main.js
@@ -2,5 +2,18 @@ const numberButtons = document.querySelectorAll('[data-number]')
 const operationButtons = document.querySelectorAll('[data-operator]')
 const equalsButton = document.querySelectorAll('[data-equals]')
 const deleteButton = document.querySelectorAll('[data-delete]')
-const previousOperand = document.querySelectorAll('[data-previous-operand]')
-const currentOperand = document.querySelectorAll('[data-current-operand]')
+const previousOperandTextElement = document.querySelectorAll('[data-previous-operand]')
+const currentOperandTextElement = document.querySelectorAll('[data-current-operand]')
+
+class Calculator {
+    constructor(previousOperandTextElement, currentOperandTextElement) {
+        this.previousOperandTextElement = previousOperandTextElement;
+        this.currentOperandTextElement = currentOperandTextElement;
+    }
+    
+    clear(){
+        this.currentOperandTextElement = '';
+        this.previousOperandTextElement = '';
+        this.operation = undefined;
+    }
+}
