97fb4291475e8b2504c3cb02881d41b499a0831d
robs-am <roberta.amaro89@gmail.com>
2022-09-15 00:42:20 -0300

    add final loading

---
+++ dia08/tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {},
+    backgroundColor: {"defaultBG": "linear-gradient(140turn, #baadfcdc, #9adade)"},
+  },
+  plugins: [],
+}
