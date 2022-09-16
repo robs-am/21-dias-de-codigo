4ddab78e7327e04b6f8427d1212e91692010efdc
robs-am <roberta.amaro89@gmail.com>
2022-09-16 08:29:34 -0300

    add stylesheet config tailwind

---
+++ dia09/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/**/*.html"],
+  theme: {
+    extend: {},
+    colors: { 
+       "base_purple":"#baadfcdc",
+        "base_green":"#9adade"
+  },
+  
+}
+}
