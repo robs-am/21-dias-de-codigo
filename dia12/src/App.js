e766dfb3f92faf8dc707a5924a0286d6c9c95d03
robs-am <roberta.amaro89@gmail.com>
2022-09-21 08:38:04 -0300

    add map method

--- dia12/src/App.js
+++ dia12/src/App.js
@@ -12,6 +12,13 @@ function App() {
     <div className='App'>
       <motion.div className="carousel">
         <motion.div className="inner-carousel">
+          {images.map(image => {
+            return (
+              <motion.div className="item">
+                <img src={image} alt="" />
+              </motion.div>
+            );
+          })}
 
         </motion.div>
       </motion.div>
