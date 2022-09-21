4ff7c5e11ee2932d46386b857e8bc8f24e89eb7b
robs-am <roberta.amaro89@gmail.com>
2022-09-21 09:28:31 -0300

    add css image

--- dia12/src/images.js
+++ dia12/src/images.js
@@ -5,6 +5,7 @@ import image3 from './assets/img03.jpg';
 import image4 from './assets/img04.jpg';
 import image5 from './assets/img05.jpg';
 import image6 from './assets/img06.jpg';
+import image7 from './assets/img07.jpg';
 
 
-export default [image1, image2, image3, image4, image5, image6];
+export default [image1, image2, image3, image4, image5, image6, image7];
