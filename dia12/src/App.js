d5f263af0b320c4eda387a1ccf6d7f13fb6949f6
robs-am <roberta.amaro89@gmail.com>
2022-09-21 07:34:57 -0300

    add assets

--- dia12/src/App.js
+++ dia12/src/App.js
@@ -1,11 +1,20 @@
-import React from 'react';
+import "./App.css";
 
-import './App.css';
+import { motion } from "framer-motion";
+import { useRef, useEffect, useState} from "react";
+import images from "./images";
+
+
 
 function App() {
+  console.log(images);
   return (
     <div className='App'>
-      <h1>Hello</h1>
+      <motion.div className="carousel">
+        <motion.div className="inner-carousel">
+
+        </motion.div>
+      </motion.div>
     </div>
   );
 }
---
+++ dia12/src/images.js
@@ -0,0 +1,10 @@
+/* eslint-disable import/no-anonymous-default-export */
+import image1 from './assets/img01.jpg';
+import image2 from './assets/img02.jpg';
+import image3 from './assets/img03.jpg';
+import image4 from './assets/img04.jpg';
+import image5 from './assets/img05.jpg';
+import image6 from './assets/img06.jpg';
+
+
+export default [image1, image2, image3, image4, image5, image6];
