2f0c94ca4268a6bc6e6409b8c13b419e2ec62088
robs-am <roberta.amaro89@gmail.com>
2022-09-21 16:58:55 -0300

    carousel grab final

--- dia12/src/App.js
+++ dia12/src/App.js
@@ -1,17 +1,26 @@
 import "./App.css";
-
 import { motion } from "framer-motion";
+//eslint-disable-next-line
 import { useRef, useEffect, useState} from "react";
 import images from "./images";
 
 
 
 function App() {
-  console.log(images);
+  const [width, setWidth] = useState(0);
+  const carousel =  useRef();
+
+  useEffect (() => {
+    
+    setWidth(carousel.current.scrollWidth - carousel.current.offsetWidth);
+  },[]);
+
   return (
     <div className='App'>
-      <motion.div className="carousel">
-        <motion.div drag="x" className="inner-carousel">
+      <motion.div ref={carousel} className="carousel" whileTap={{cursor:"grabbing"}}>
+        <motion.div 
+        drag= "x" 
+        dragConstraints={{right:0, left: -width}} className="inner-carousel">
           {images.map(image => {
             return (
               <motion.div className="item">
