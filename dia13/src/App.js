6bcb10780bbb9cb9f5cef395bc220f680f46e727
robs-am <roberta.amaro89@gmail.com>
2022-09-23 02:49:43 -0300

    add flexbox navbar

--- dia13/src/App.js
+++ dia13/src/App.js
@@ -1,11 +1,6 @@
 import React from "react";
 // eslint-disable-next-line
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 import Navbar from "./components/Navbar";
 import Home from "./components/Home";
@@ -13,19 +8,30 @@ import Projects from "./components/Projects";
 import Skills from "./components/Skills";
 import Contact from "./components/Contact";
 
-
-
 function App() {
   return (
     <>
-      <Navbar />
       <Router>
-        <Switch>
-          <Route path='/' components ={Home} exact> <Home /></Route>
-          <Route path='/projects' components ={Projects} exact> <projects /></Route>
-          <Route path='/contact' components ={Contact} exact> <contact /></Route>
-          <Route path='/skills' components ={Skills} exact> <skills /></Route>
-        </Switch>
+        <Navbar />
+
+          <Switch>
+            <Route path="/home" components={Home} exact>
+              {" "}
+              <Home />
+            </Route>
+            <Route path="/projects" components={Projects} exact>
+              {" "}
+              <Projects />
+            </Route>
+            <Route path="/contact" components={Contact} exact>
+              {" "}
+              <Contact />
+            </Route>
+            <Route path="/skills" components={Skills} exact>
+              {" "}
+              <Skills />
+            </Route>
+          </Switch>
       </Router>
     </>
   );
--- dia13/src/components/Home.js
+++ dia13/src/components/Home.js
@@ -4,7 +4,7 @@ const Home = () => {
     return (
      
         <section className='hero'>
-        <h1>Home</h1>
+        <h1>Welcome</h1>
         </section>
   
      
