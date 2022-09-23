5020538ad6f67ccdac200763320ddd4d396a3bae
robs-am <roberta.amaro89@gmail.com>
2022-09-23 09:45:37 -0300

    add card

--- dia13/src/components/Navbar.js
+++ dia13/src/components/Navbar.js
@@ -2,29 +2,54 @@ import React from "react";
 import { Link } from "react-router-dom";
 import "./navbar.css";
 import { FaBars } from "react-icons/fa";
+import Perfil from "./images.js";
 
 const Navbar = () => {
   return (
-    <nav className="navbar">
-      <ul className="nav-links">
-        <Link to="/home">
-          <li>Início</li>
-        </Link>
-        <Link to="/contact">
-          <li>Contato</li>
-        </Link>
-        <Link to="/projects">
-          <li>Projetos</li>
-        </Link>
-        <Link to="/skills">
-          <li>Habilidades</li>
-        </Link>
-      </ul>
+    <>
+      <nav className="navbar">
+        <ul className="nav-links">
+          <Link to="/home">
+            <li>Início</li>
+          </Link>
+          <Link to="/contact">
+            <li>Contato</li>
+          </Link>
+          <Link to="/projects">
+            <li>Projetos</li>
+          </Link>
+          <Link to="/skills">
+            <li>Habilidades</li>
+          </Link>
+        </ul>
 
-      <button className="mobile-menu-icon">
-        <FaBars />
-      </button>
-    </nav>
+        <button className="mobile-menu-icon">
+          <FaBars />
+        </button>
+      </nav>
+      <section className="hero">
+        <div className="card-perfil">
+          <div className="perfil">
+            <Perfil />
+          </div>
+          <a
+            href="http://linkedin.com/roberta-amaro"
+            rel="noreferrer"
+            target="_blank"
+          >
+            Linkedin
+          </a>
+          <a
+            href="https://github.com/robs-am"
+            rel="noreferrer"
+            target="_blank"
+          >
+            Github
+          </a>
+          <button onClick={() => window.location = 'mailto:roberta.amaro89l@gmail.com'}>Contact Me</button>
+        </div>
+      </section>
+    </>
   );
 };
 
