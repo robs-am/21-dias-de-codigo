7b0d0f0a115eb9ee87821341ca2d4a0c74956a25
robs-am <roberta.amaro89@gmail.com>
2022-09-27 08:25:47 -0300

    estruttura pagina welcome

--- dia14/src/components/Welcome/Welcome.jsx
+++ dia14/src/components/Welcome/Welcome.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import './Welcome.css'
 
 const Welcome = () => {
     return(
-        <div>Welcome</div>
+        <div>
+            <h2>Seja bem-vindo</h2>
+            <p>Clique no botão abaixo para começar:</p>
+            <button>Iniciar</button>
+            < img src="" alt="" />
+        </div>
     )
 }
 
