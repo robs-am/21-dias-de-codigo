f34bac8760399d7a91d4dba2e1c1c1607d9aa086
robs-am <roberta.amaro89@gmail.com>
2022-09-28 09:38:11 -0300

    estrutura question page

--- dia14/src/components/Question/Question.jsx
+++ dia14/src/components/Question/Question.jsx
@@ -3,8 +3,14 @@ import "./Question.css"
 
 const Question = () => {
     return (
-        <div>Question</div>
-    )
+        <div>
+            <p>Pergunta de 0 a 10</p>
+            <h2>Pergunta atual</h2>
+            <div id="options-container">
+                <p>Opções</p>
+            </div>
+        </div>
+    );
 };
 
 export default Question
