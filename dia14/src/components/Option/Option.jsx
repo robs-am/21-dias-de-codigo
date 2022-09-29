c1a52889142bb9f60c28c07616ea979fbb961771
robs-am <roberta.amaro89@gmail.com>
2022-09-29 00:24:48 -0300

    add Option.jsx imports

---
+++ dia14/src/components/Option/Option.jsx
@@ -0,0 +1,12 @@
+import { useContext } from "react";
+import { QuizContext } from "../../context/quiz";
+
+import "./Option.css"
+
+const Option = () => {
+    return (
+        <div>Option</div>
+    );
+}
+
+export default Option;
