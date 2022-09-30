7aa638b4d65556bc0d57ea39371f547c1f48d193
robs-am <roberta.amaro89@gmail.com>
2022-09-30 12:40:03 -0300

    dia14

--- dia14/src/components/Option/Option.jsx
+++ dia14/src/components/Option/Option.jsx
@@ -5,12 +5,16 @@ import "./Option.css";
 
 const Option = ({ option, selectOption, answer }) => {
   const [quizState, dispatch] = useContext(QuizContext);
+  
 
   return (
-    <div className={`option ${quizState.answerSelected && option === answer ? 'correct' : ""}
-    ${quizState.answerSelected && option !== answer ? 'wrong' : ""}`}
-   
-    onClick={() => selectOption()}>
+    <div
+      className={`option ${
+        quizState.answerSelected && option === answer ? "correct" : ""
+      }
+    ${quizState.answerSelected && option !== answer ? "wrong" : ""}`}
+      onClick={() => selectOption()}
+    >
       <p>{option}</p>
     </div>
   );
