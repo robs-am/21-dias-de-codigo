330f0ae45307b1149c86837a8d5342eedad7a43c
robs-am <roberta.amaro89@gmail.com>
2022-09-29 22:04:56 -0300

    change button correct/wrong olors

--- dia14/src/components/Option/Option.jsx
+++ dia14/src/components/Option/Option.jsx
@@ -7,7 +7,10 @@ const Option = ({ option, selectOption, answer }) => {
   const [quizState, dispatch] = useContext(QuizContext);
 
   return (
-    <div className="option" onClick={() => selectOption()}>
+    <div className={`option ${quizState.answerSelected && option === answer ? 'correct' : ""}
+    ${quizState.answerSelected && option !== answer ? 'wrong' : ""}`}
+   
+    onClick={() => selectOption()}>
       <p>{option}</p>
     </div>
   );
