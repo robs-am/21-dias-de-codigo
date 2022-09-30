6e5423b07689bcaf1c1d536a17abf42cc19b2663
robs-am <roberta.amaro89@gmail.com>
2022-09-30 02:56:41 -0300

    add deploy verel ultimos ajustes

--- dia14/vite.config.js
+++ dia14/vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { ServerStyleSheets } from '@material-ui/core'
+/* import { ServerStyleSheets } from '@material-ui/core' */
 
 // https://vitejs.dev/config/
 export default defineConfig({
