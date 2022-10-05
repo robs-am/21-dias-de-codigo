d9e076a91921cb2fd4af73aff1b89169ed573f5c
robs-am <roberta.amaro89@gmail.com>
2022-10-04 22:19:58 -0300

    import bootstrap e browser route no main.jsx

--- dia15/src/main.jsx
+++ dia15/src/main.jsx
@@ -2,9 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 
+import { BrowserRouter } from 'react-router-dom';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import '../node_modules/bootstrap/dist/js/bootstrap.bundle'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  <BrowserRouter>
     <App />
-  </React.StrictMode>
+  </BrowserRouter>
 )
