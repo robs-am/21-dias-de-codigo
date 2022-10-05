2e9f4ad672d523172849aef793702828249832eb
robs-am <roberta.amaro89@gmail.com>
2022-10-04 22:23:37 -0300

    add pages about contact home products

--- dia15/src/main.jsx
+++ dia15/src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
+import App from './pages/Home'
 
 import { BrowserRouter } from 'react-router-dom';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
