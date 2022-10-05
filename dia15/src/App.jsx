0b01fdadcd739ba5151a4d8fc3fdc3b2b1d0463a
robs-am <roberta.amaro89@gmail.com>
2022-10-04 23:08:37 -0300

    add switch and route

--- dia15/src/App.jsx
+++ dia15/src/App.jsx
@@ -5,12 +5,17 @@ import Home from './pages/Home';
 import About from './pages/About';
 import Contact from './pages/Contact';
 import Product from './pages/Product';
+import { Switch } from 'react-router-dom';
 
 import React from 'react'
 
 const App = () => {
   return (
     <>
+    <Switch>
+        <Route>
+        </Route> 
+    </Switch>
     </>
   )
 }
