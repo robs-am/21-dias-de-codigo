8eb93de36d0ca0670372b2777e723367579d7198
robs-am <roberta.amaro89@gmail.com>
2022-10-06 15:12:06 -0300

    criando mobile

--- dia15/src/App.jsx
+++ dia15/src/App.jsx
@@ -4,16 +4,22 @@ import Header from "./components/header/header";
 import MainContent from "./components/mainContext/mainContent";
 import Footer from "./components/footer";
 import Banner from "./assets/banner.png"
+import { useState } from "react";
 /* import Menina2 from "./assets/menina2.mp4"; */
 
 function App() {
+  const[menuIsVisible, setMenuisVisible] = useState(true);
+
   return (
     <div className="container">
       <img classname="banner" src={Banner} alt="banner mulher headphone" />
       {/* <video className="video" src={Menina2} autoPlay loop /> */}
 
       <Header />
-      
+      <MenuMobile 
+      menuIsVisible={menuIsVisible}
+      setMenuisVisible={setMenuisVisible}
+      />
       <MainContent />
       <Footer />
     </div>
--- dia15/src/components/header/header.jsx
+++ dia15/src/components/header/header.jsx
@@ -1,38 +1,38 @@
-import React from 'react';
-import './header.css';
+import React from "react";
+import "./header.css";
 import { useState } from "react";
-import { FaBars } from 'react-icons/fa'
-import { ImCross } from "react-icons/im";
-
-
+/* import { FaBars } from "react-icons/fa";
+import { ImCross } from "react-icons/im"; */
 
 const Header = () => {
-  const [Mobile, setMobile] = useState(false)
+  
 
   return (
     <div>
-      <nav className='navbar'>
-        <ul className= {Mobile ? "nav-links-mobile": "nav-links"} onClick ={() => setMobile(false)}>
-          <li className='nav-links'>Home</li>
-          <li className='nav-links'>Headphones</li>
-          <li className='nav-links'>Speakers</li>
-          <li className='nav-links'>Earbuds</li>
+      <nav className="navbar">
+        <ul
+          
+        >
+          <li>Home</li>
+          <li>Headphones</li>
+          <li>Speakers</li>
+          <li>Earbuds</li>
           <div className="search">
-          <i className="fa fa-search"></i>
-          <i className="fa fa-shopping-basket"></i>
-        </div>
+            <i className="fa fa-search"></i>
+            <i className="fa fa-shopping-basket"></i>
+          </div>
         </ul>
-        <button className="mobile-menu-icon" onClick={() => setMobile(!Mobile)}>{Mobile? <ImCross /> : <FaBars />}
-          
-          
-          </button>
-        
-          
-       
+        <div classname="mobile-button">
+         {/*  <button
+            className="navbar-mobile-menu-icon"
+            onClick={() => setMobile(!Mobile)}
+          >
+            {Mobile ? <ImCross /> : <FaBars />}
+          </button> */}
+        </div>
       </nav>
     </div>
-  )
-}
+  );
+};
 
-export default Header
-
+export default Header;
