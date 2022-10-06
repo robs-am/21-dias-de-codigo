7fb5520fad3c8775dfa18b0457f22473a14b7d05
robs-am <roberta.amaro89@gmail.com>
2022-10-05 21:11:19 -0300

    add hover

--- dia15/src/components/data/product_data.jsx
+++ dia15/src/components/data/product_data.jsx
@@ -5,7 +5,7 @@ const product_card = [
         description: "A best-in-class combination of noise cancelling, audio performance, and comfort",
         price:329,
         currency: "$",
-        thumb:"./img/img01.jpeg"
+        thumb:"./img/img01.png"
     },
 
     {
@@ -18,6 +18,31 @@ const product_card = [
     },
     {
         id:3,
+        product_name: "SoundLink Micro Bluetooth® speaker Bundles",
+        description: "Waterproof, dustproof, extreme-temperature proof. Adventure ready",
+        price:202,
+        currency: "$",
+        thumb:"./img/img03.png"
+    },
+    {
+        id:4,
+        product_name: "Bose QuietComfort® 45 headphones",
+        description: "A best-in-class combination of noise cancelling, audio performance, and comfort",
+        price:329,
+        currency: "$",
+        thumb:"./img/img01.png"
+    },
+
+    {
+        id:5,
+        product_name: "S1 Pro Portable Bluetooth® speaker system",
+        description: "Be the hero of the party. This Bluetooth® speaker delivers big sound from a rugged, all-in-one system.",
+        price:699,
+        currency: "$",
+        thumb:"./img/img02.png"
+    },
+    {
+        id:6,
         product_name: "SoundLink Micro Bluetooth® speaker Bundles",
         description: "Waterproof, dustproof, extreme-temperature proof. Adventure ready",
         price:202,
