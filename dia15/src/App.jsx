4391b54ce8fa71c81d908abcba444db99b68f17b
robs-am <roberta.amaro89@gmail.com>
2022-10-07 18:16:21 -0300

    add import switch and routers

--- dia15/src/App.jsx
+++ dia15/src/App.jsx
@@ -8,8 +8,7 @@ import Banner from "./assets/banner.png";
 import Speakers from "./components/Speakers";
 import Earbuds from "./components/Earbuds";
 import Headphones from "./components/Headphones";
-import { Router, Switch } from "react-router-dom";
-
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 /* import Menina2 from "./assets/menina2.mp4"; */
 
@@ -20,9 +19,28 @@ function App() {
       {/* <video className="video" src={Menina2} autoPlay loop /> */}
       <Router>
         <Header />
-          <Switch>
-        <MainContent />
-        <Footer />
+        <Switch>
+          <Route path="/" components={Home} exact>
+            {" "}
+            <Home />
+          </Route>
+
+          <Route path="/" components={Headphones} exact>
+            {" "}
+            <Headphones />
+          </Route>
+
+          <Route path="/" components={Speakers} exact>
+            {" "}
+            <Speakers />
+          </Route>
+
+          <Route path="/" components={Earbuds} exact>
+            {" "}
+            <Earbuds />
+          </Route>
+          <MainContent />
+          <Footer />
         </Switch>
       </Router>
     </div>
