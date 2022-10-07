4540777e4c285156baf335e3f1e15b2bcd4f836c
robs-am <roberta.amaro89@gmail.com>
2022-10-07 15:03:48 -0300

    add novo menu mobile

--- dia15/src/components/header/header.jsx
+++ dia15/src/components/header/header.jsx
@@ -3,6 +3,7 @@ import './header.css';
 import { useState } from "react";
 import { FaBars } from 'react-icons/fa'
 import { ImCross } from "react-icons/im";
+import { Link } from 'react-router-dom';
 
 
 
@@ -13,16 +14,17 @@ const Header = () => {
     <div>
       <nav className='navbar'>
         <ul className= {Mobile ? "nav-links-mobile": "nav-links"} onClick ={() => setMobile(false)}>
-          <li className='nav-links'>Home</li>
-          <li className='nav-links'>Headphones</li>
-          <li className='nav-links'>Speakers</li>
-          <li className='nav-links'>Earbuds</li>
+         
+          <li className='nav-links-mobile-hover'>Home</li>
+          <li className='nav-links-mobile-hover'>Headphones</li>
+          <li className='nav-links-mobile-hover'>Speakers</li>
+          <li className='nav-links-mobile-hover'>Earbuds</li>
           <div className="search">
           <i className="fa fa-search"></i>
           <i className="fa fa-shopping-basket"></i>
         </div>
         </ul>
-        <button className="mobile-menu-icon" onClick={() => setMobile(!Mobile)}>{Mobile? <ImCross /> : <FaBars />}
+        <button className="mobile-menu-icon" onClick={() => setMobile(!Mobile)}>{Mobile? <ImCross size={25} /> : <FaBars size={25} />}
           
           
           </button>
