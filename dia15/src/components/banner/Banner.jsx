f294e5fc6612b9fe70861a934053c6447633f854
robs-am <roberta.amaro89@gmail.com>
2022-10-08 21:53:49 -0300

    add banner animation

--- dia15/src/components/banner/Banner.jsx
+++ dia15/src/components/banner/Banner.jsx
@@ -3,7 +3,10 @@ import './banner.css'
 
 const Banner = () => {
   return (
-    <div className='banner'></div>
+    <div className='banner'>
+      <h1 className='title'>Keep your music playing</h1>
+      <h2 className='subtitle'>Better sound for any activity and ocasion</h2>
+      </div>
   )
 }
 
